1060ac7e9d35ff68bfd3e12523b6ba8473cbc917
Thierrix <thierry.sokhn@epfl.ch>
2024-02-27 12:07:49 +0100

    Updating Player.py
    Adding the update of real player

--- Player.py
+++ Player.py
@@ -26,10 +26,32 @@ class AIPlayer:
         
         #Sort the array of piece according to the biggest coords (x, y)
         self.pieces_array.sort(key=lambda piece: piece.coords[1]**2 + piece.coords[0]**2, reverse=True)
-            
+        
+        #a arranger
         for piece in self.pieces_array:
+            
             #Check if the piece can move
+                if piece.can_move():
+                    #Move the piece
+                    piece.move()
+                    break
+    
 
-                #Move the piece
-                piece.move()
-                return piece.get_coords() 
+    def real_move(self): 
+        #a arranger tres probablement
+
+        #Choosing which piece to move
+        piece_to_move = input("Which piece do you want to move ?")
+        while piece_to_move not in self.pieces_array:
+            piece_to_move = input("Which piece do you want to move ? (please enter a valid piece) : ")
+        
+        #Computing the legal moves for the piece
+        moves = board.legal_moves(piece_to_move)
+
+        
+        move = input("Where do you want to move it ?")
+        while move not in moves:
+            move = input("Where do you want to move it ? (please enter a valid move  in the good format) : ")
+
+        piece_to_move.set_coords(move)
+    
