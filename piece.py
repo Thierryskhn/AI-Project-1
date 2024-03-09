8bd9815bc5a5a5cb46efb97022dcd49259fedef0
s231865 <s231865@dtu.dk>
2024-03-09 10:16:30 +0100

    create pieces at the start

--- piece.py
+++ piece.py
@@ -1,3 +1,16 @@
 class Piece:
-    def __init__(self,player,x,y):
-        pass
+
+    def __init__(self, name, color, coords):
+        self.color  = color
+        self.coords = coords
+        self.name = name
+
+
+    def __str__(self):
+        return self.name
+    
+    def get_coords(self):
+        return self.coords
+    
+    def set_coords(self, coords):
+        self.coords = coords
