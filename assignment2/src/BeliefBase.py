41691bf74b5684744095f4dd5f4bbb5a0e5645ad
Nathan Gromb <nathan.gromb@epfl.ch>
2024-04-16 11:12:49 +0200

    renamed belief set (now belief base)

--- assignment2/src/BeliefBase.py
+++ assignment2/src/BeliefBase.py
@@ -1,21 +1,21 @@
 from Belief import Belief, Not, And, Or, If, Iff
 
-class BeliefSet:
-    """ Represents a set of beliefs. """
+class BeliefBase:
+    """ Represents a belief base. """
     
     def __init__(self, *beliefs: Belief) -> None:
         self.beliefs = set(beliefs)
 
     def add_belief(self, belief: Belief) -> None:
-        """ Adds a belief to the belief set. """
+        """ Adds a belief to the belief base. """
         self.beliefs.add(belief)
 
     def remove_belief(self, belief: Belief) -> None:
-        """ Removes a belief from the belief set. """
+        """ Removes a belief from the belief base. """
         self.beliefs.remove(belief)
 
     def get_beliefs(self) -> set:
-        """ Returns the beliefs in the belief set. """
+        """ Returns the beliefs in the belief base. """
         return self.beliefs
 
     def __str__(self) -> str:
@@ -28,12 +28,12 @@ def main():
     b = Belief("b")
     c = Or(a, b)
     d = Belief("d")
-    belief_set = BeliefSet(a, b, c)
-    print(belief_set)
-    belief_set.add_belief(d)
-    print(belief_set)
-    belief_set.remove_belief(a)
-    print(belief_set)
+    belief_base = BeliefBase(a, b, c)
+    print(belief_base)
+    belief_base.add_belief(d)
+    print(belief_base)
+    belief_base.remove_belief(a)
+    print(belief_base)
 
     print()
 
